40b8c9cfdf57ed7499020e14116715a7b6cb9107
Johannspn <s.shamsi2020@gmail.com>
2022-05-30 14:45:41 +0430

    video03

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="header">
+        {/* <div className="brand">
+          <button onClick={openMenu}>&#9776;</button>
+          <Link to="/">amazona</Link>
+        </div>
+        <div className="header-links">
+          <Link to="/cart">Cart</Link>
+          {userInfo ? (
+            <Link to="/profile">info.name</Link>
+          ) : (
+            <Link to="/signin">Sign In</Link>
+          )}
+        </div> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
